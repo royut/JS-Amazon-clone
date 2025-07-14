5083665131b4090f4138083dc119678f5f3eb0a6
Royut <mohammadzareei36@gmail.com>
2025-07-14 00:44:43 -0700

    loading products in page - issues fixed

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -21,7 +21,7 @@ products.forEach((product) => {
             </div>
         
             <div class="product-price">
-                $${Number(product.priceCents)/100}
+                $${(Number(product.priceCents)/100).toFixed(2)}
             </div>
         
             <div class="product-quantity-container">
