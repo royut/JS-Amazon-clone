967c9a6128ae6b576b91026463d6a4377a6df41a
Royut <mohammadzareei36@gmail.com>
2025-07-16 19:04:58 -0700

    default value for delivery options

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -23,5 +23,5 @@ export function getDeliveryOption (deliveryOptionId) {
             matchingDeliveryOption = deliveryOption
         }
     })
-    return matchingDeliveryOption
+    return matchingDeliveryOption || deliveryOptions[0]
 }
