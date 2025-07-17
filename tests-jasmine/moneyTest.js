36465f24ef286a27cebada5ce9b9bb31acd84e81
Royut <mohammadzareei36@gmail.com>
2025-07-16 23:01:03 -0700

    added test with jasmine

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,16 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+describe('test suite: formatCurrency', () => {
+    it('convert cents to dollars:', () => {
+        expect(formatCurrency(2095)).toBe('20.95')
+    })
+    it('works with 0', () =>{
+        expect(formatCurrency(0)).toBe('0.00')
+    })
+    it('round up to nearest cent', () => {
+        expect(formatCurrency(2000.5)).toBe('20.01')
+    })
+    it('round down to nearest cent', () => {
+        expect(formatCurrency(2000.49)).toBe('20.00')
+    })
+})
