ff04ef1d18380f41a32c208e4a31f7df3568bc61
Royut <mohammadzareei36@gmail.com>
2025-07-17 21:15:19 -0700

    create class version of cart

---
+++ data/cart-class.js
@@ -0,0 +1,61 @@
+class Cart {
+    cartItems
+    locaStorageKey
+
+    constructor(locaStorageKey) {
+        this.locaStorageKey = locaStorageKey
+        this.loadFromStorage()
+    }
+    // load from local storage
+    loadFromStorage() {
+        this.cartItems = JSON.parse(localStorage.getItem(this.locaStorageKey)) || []
+    }
+    // save to local storage
+    saveToLocalStorage() {
+        localStorage.setItem(this.locaStorageKey, JSON.stringify(this.cartItems))
+    }
+    // add product to the cart
+    addToCart(productId) {
+        let mathingItem
+        this.cartItems.forEach(cartItem => {
+            if (cartItem.productId === productId) {
+                mathingItem = cartItem
+            }
+        })
+        if (mathingItem) {
+            mathingItem.quantity += 1
+        } else {
+            this.cartItems.push({
+                productId,
+                quantity: 1,
+                deliveryOptionId: '1'
+            })
+        }
+        this.saveToLocalStorage()
+    }
+    // remove product from the cart
+    removeFromCart(productId) {
+        this.cartItems.forEach((cartItem, index) => {
+            if (cartItem.productId === productId) {
+                this.cartItems.splice(index, 1)
+            }
+        })
+        this.saveToLocalStorage()
+    }
+    // update delivery option
+    updateDeliveryOption(productId, newDeliveryOptionId) {
+        this.cartItems.forEach(cartItem => {
+            if (cartItem.productId === productId) {
+                cartItem.deliveryOptionId = newDeliveryOptionId
+            }
+        })
+        this.saveToLocalStorage()
+    }
+}
+
+const cart = new Cart('cart-oop');
+const businessCart = new Cart('cart-business')
+
+console.log(cart)
+console.log(businessCart)
+console.log(businessCart instanceof Cart)
