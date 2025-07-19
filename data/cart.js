9afff8f16de5cea8c9069c64536b2bb8b4e36c10
Royut <mohammadzareei36@gmail.com>
2025-07-18 22:51:11 -0700

    used promises and convert to promises

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,5 @@
+import {products} from "./products.js";
+
 export let cart;
 
 loadFromStorage()
@@ -50,4 +52,17 @@ export function updateDeliveryOption (productId, newDeliveryOptionId) {
         }
     })
     saveToLocalStorage()
+}
+
+
+export function loadCart (fun) {
+    const xhr = new XMLHttpRequest()
+
+    xhr.addEventListener('load', () => {
+        console.log(xhr.response)
+        fun();
+    })
+
+    xhr.open('GET', 'https://supersimplebackend.dev/cart')
+    xhr.send()
 }
