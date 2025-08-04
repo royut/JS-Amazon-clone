86830d906a455276e6416604d116cab3eb8f0ac5
Royut <mohammadzareei36@gmail.com>
2025-08-03 18:19:12 -0700

    test: add tests for `removeFromCart` functionality, covering both existing and non-existing product cases

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import {addToCart, cart, loadFromStorage} from '../../data/cart.js';
+import {addToCart, removeFromCart, cart, loadFromStorage} from '../../data/cart.js';
 
 describe('test suite: addToCart', () => {
     beforeEach(() => {
@@ -51,5 +51,50 @@ describe('test suite: addToCart', () => {
                 deliveryOptionId: '1'
             }
         ]))
+    })
+
+    it('remove an existing product from the cart', () => {
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([
+                {
+                    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                    quantity: 1,
+                    deliveryOptionId: '1'
+                }
+            ])
+        })
+        loadFromStorage()
+
+        removeFromCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6')
+        expect(cart.length).toEqual(0)
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1)
+        expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([]))
+        expect(cart).toEqual([])
+    })
+
+    it('remove a non-existing product from the cart', () => {
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([
+                {
+                    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                    quantity: 1,
+                    deliveryOptionId: '1'
+                }
+            ])
+        })
+        loadFromStorage()
+
+        removeFromCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c7')
+        expect(cart.length).toEqual(1)
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1)
+        expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([
+            {
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 1,
+                deliveryOptionId: '1'
+            }
+        ]))
+        expect(cart[0].quantity).toEqual(1)
+        expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6')
     })
 })
