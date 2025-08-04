c12f7253ab2f37091937695a06430d5799abde22
Royut <mohammadzareei36@gmail.com>
2025-08-03 17:43:19 -0700

    refactor: enhance test coverage for cart and order summary, add `beforeEach`/`afterEach` hooks and improve assertions

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,6 +1,6 @@
 import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
 import {loadFromStorage, cart} from '../../data/cart.js';
-import {loadProducts, loadProductsFetch} from '../../data/products.js';
+import {getProduct, loadProducts, loadProductsFetch} from '../../data/products.js';
 
 describe('test suite: renderOrderSummary', () => {
     const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'
@@ -16,6 +16,7 @@ describe('test suite: renderOrderSummary', () => {
         // set up a test environment
         spyOn(localStorage, 'setItem')
         document.querySelector('.js-test-container').innerHTML = `
+            <div class="checkout-header"></div>
             <div class="order-summary"></div>
             <div class="payment-summary"></div>
         `
@@ -34,9 +35,14 @@ describe('test suite: renderOrderSummary', () => {
             ])
         })
         loadFromStorage()
-
         renderOrderSummary()
     })
+
+    afterEach(() => {
+        // clear the page
+        document.querySelector('.js-test-container').innerHTML = ''
+    })
+
     // test: how the page looks
     it('displays the cart', () => {
         // tests-simple
@@ -49,8 +55,18 @@ describe('test suite: renderOrderSummary', () => {
         expect(
             document.querySelector(`.js-product-quantity-${productId2}`).innerText
         ).toContain('Quantity: 2')
-        // clear the page
-        document.querySelector('.js-test-container').innerHTML = ''
+        expect(
+            document.querySelector(`.js-cart-item-container-${productId1} > .cart-item-details-grid > .cart-item-details > .product-name`).innerText
+        ).toEqual(getProduct(productId1).name)
+        expect(
+            document.querySelector(`.js-cart-item-container-${productId2} > .cart-item-details-grid > .cart-item-details > .product-name`).innerText
+        ).toEqual(getProduct(productId2).name)
+        expect(
+            document.querySelector(`.js-cart-item-container-${productId1} > .cart-item-details-grid > .cart-item-details > .product-price`).innerText[0]
+        ).toBe('$')
+        expect(
+            document.querySelector(`.js-cart-item-container-${productId2} > .cart-item-details-grid > .cart-item-details > .product-price`).innerText[0]
+        ).toBe('$')
     })
     // test: how the page behaves
     it('removes a product from the cart', () => {
@@ -68,7 +84,5 @@ describe('test suite: renderOrderSummary', () => {
         ).not.toEqual(null)
         expect(cart.length).toEqual(1)
         expect(cart[0].productId).toEqual(productId2)
-        // clear the page
-        document.querySelector('.js-test-container').innerHTML = ''
     })
 })
--- tests/tests-simple/moneyTest.js
+++ tests/tests-simple/moneyTest.js
@@ -33,3 +33,11 @@ if (formatCurrency(2000.4) === '20.00'){
 else {
     console.log('failed')
 }
+
+console.log('negative number')
+if (formatCurrency(-2000.4) === '-20.00'){
+    console.log('passed')
+}
+else {
+    console.log('failed')
+}
